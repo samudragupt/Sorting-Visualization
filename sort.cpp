5c1feb9ee43d111bab247004089dad0e39ddddfc
samudragupt <48641735+samudragupt@users.noreply.github.com>
2020-03-15 01:18:27 +0530

    Final Project

--- sort.cpp
+++ sort.cpp
@@ -3,7 +3,7 @@
 #include<unistd.h>
 using namespace std;
 #define ll long long
-void display(vector<vector<char>>vect,ll k)
+void display(vector<vector<char>>&vect,ll k)
 {
 	usleep(k*40000);
 	system("clear");
@@ -15,7 +15,7 @@ void display(vector<vector<char>>vect,ll k)
 	}
 	return;
 }
-void bubble(vector<vector<char>>vect)
+void bubble(vector<vector<char>>&vect)
 {
 	ll n=vect.size();
 	for(ll i=0; i<n-1;i++)
@@ -33,7 +33,7 @@ void bubble(vector<vector<char>>vect)
 		}
 	}
 }
-void selection(vector<vector<char>>vect)
+void selection(vector<vector<char>>&vect)
 {
 	ll n=vect.size();
 	for(ll i=0;i<n-1;i++)
@@ -54,7 +54,7 @@ void selection(vector<vector<char>>vect)
 	}
 	//display(vect,n);
 }
-void insertion(vector<vector<char>>vect)
+void insertion(vector<vector<char>>&vect)
 {
 	ll n=vect.size();
 	for(ll i=1;i<n;i++)
@@ -69,10 +69,56 @@ void insertion(vector<vector<char>>vect)
 		}
 	}
 }
-void mergesort(vector<vector<char>>vect)
+void merge(vector<vector<char>>&vect, ll start, ll mid, ll end )
+{
+	vector<vector<char>>sect1(mid-start+1),sect2(end-mid);
+	for(ll i=start;i<=mid;i++)
+	sect1[i-start]=vect[i];
+	for(ll i=mid+1;i<=end;i++)
+	sect2[i-mid-1]=vect[i];
+	ll i=start,j=0,k=0;
+	while(j<sect1.size() && k<sect2.size())
+	{
+		if(sect1[j].size()<=sect2[k].size())
+		{
+			vect[i]=sect1[j];
+			i++;
+			j++;
+		}
+		else
+		{
+			vect[i]=sect2[k];
+			i++;
+			k++;
+		}
+		display(vect,3);
+	}
+	while(j<sect1.size())
+	{
+		vect[i]=sect1[j];
+		i++;
+		j++;
+		display(vect,3);
+	}
+	while(k<sect2.size())
+	{
+		vect[i]=sect2[k];
+		k++;
+		i++;
+		display(vect,3);
+	}
+}
+void mergesort(vector<vector<char>>&vect,ll start, ll end )
 {
 	ll n=vect.size();
-	
+	if(start<end)
+	{
+		ll mid=(start+end)/2;
+		mergesort(vect,start,mid);
+		mergesort(vect,mid+1,end);
+		merge(vect,start,mid,end);
+		// display(vect,n);
+	}
 }
 /*
 bool comp(vector<char>A, vector<char>B)
@@ -124,12 +170,14 @@ int main()
 	selection(vect);
 	else if(choice==3)
 	insertion(vect);
+	else if(choice==4)
+	mergesort(vect,0,vect.size()-1);
 	else
 	cout<<"wrong output\n"<<endl;
-	cout<<"1. continue?";
-	cin>>choice;
-	if(choice==1)
-	goto menu;
+	// cout<<"1. continue?";
+	// cin>>choice;
+	// if(choice==1)
+	// goto menu;
 	// cout<<"After sorting\n";
 	// display(vect);
 	return 0;
